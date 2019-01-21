7774d5bbc4d4ee0a0c8c7ce5267208ee921d99a4
haraja <hrajalin@hotmail.com>
2019-01-21 13:20:36 +0100

    commit to fix omitting main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,3 @@
-//
-// Arduino Uno project
-//
-// Game to practice numbers and speed
-// - display shows 1..9 lines. User needs to select same number with keypad
-// - program displays the speed for the number selection
-// - intended for child who is learning basic numbers
-//
-// circuit: https://www.tinkercad.com/things/cR6FBUxwbxK-knowyournumber
-
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include <IRremote.h>
